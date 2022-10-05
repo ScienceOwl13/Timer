3777215afb6845184c5bc3ae2e35ffb99daaca83
Connor Groen <71343047+ScienceOwl13@users.noreply.github.com>
2022-10-04 20:27:35 -0400

    First Working
    Makes a progress ring that changes depending on the value: need to connect it with the actual timer.

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,18 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Connor Groen on 2022-10-04.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_menu_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
